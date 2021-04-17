02bae884e15e75c19b197fa5f89d95367004aebf
arturo <anegreiross@outlook.com>
2021-04-16 19:39:09 -0500

    created package nn

---
+++ nn/NeuronalNetwork.go
@@ -0,0 +1,26 @@
+package nn
+
+import "fmt"
+
+func GradientDescent() int {
+	return 0
+}
+
+// Metrics
+
+func EuclidianDistance() int {
+	return 0
+}
+
+func CosinDistance() int {
+	return 0
+}
+
+func ManhattanDistance() int {
+	return 0
+}
+
+func Greeting() {
+
+	fmt.Println("Hi")
+}
