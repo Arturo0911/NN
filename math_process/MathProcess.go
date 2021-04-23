7a03ba50a7bd21c7cc026be394f2e4a260b82375
arturo <anegreiross@outlook.com>
2021-04-23 12:51:53 -0500

    ok

--- math_process/MathProcess.go
+++ math_process/MathProcess.go
@@ -11,7 +11,7 @@ import (
 // Body of parameters
 
 //
-//.d8888b. 888           888   d8b        888   d8b                8888888b.                                     888
+//.d8888b. 888           888   d8b        888   d8b                 8888888b.                                     888
 //d88P  Y88b888           888   Y8P        888   Y8P                888   Y88b                                    888
 //Y88b.     888           888              888                      888    888                                    888
 // "Y888b.  888888 8888b. 888888888.d8888b 888888888 .d8888b.d8888b 888   d88P 8888b. 888d88888888b.d88b.  .d88b. 888888 .d88b. 888d888.d8888b
@@ -20,10 +20,7 @@ import (
 // Y88b  d88PY88b. 888  888Y88b. 888     X88Y88b. 888Y88b.        X88888       888  888888    888  888  888Y8b.    Y88b. Y8b.    888         X88
 // "Y8888P"  "Y888"Y888888 "Y888888 88888P' "Y888888 "Y8888P 88888P'888       "Y888888888    888  888  888 "Y8888  "Y888 "Y8888 888     88888P'
 
-type MathModel struct {
-	BetaOne  float64
-	BetaZero float64
-	Bias     float64
+type MathModel interface {
 }
 
 type StatisticsParameters struct {
@@ -31,6 +28,11 @@ type StatisticsParameters struct {
 	CorrelationCoefficient float64
 	Average                float64
 	Variance               float64
+	Sx                     float64
+	Sy                     float64
+	BetaOne                float64
+	BetaZero               float64
+	Bias                   float64
 }
 
 type NeuronalNetwork struct {
