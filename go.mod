056c79546c2d06688987cf7113cfa42d4aba84a2
arturo <anegreiross@outlook.com>
2021-05-18 17:13:57 -0500

    still building neuron

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Arturo0911/NN
 
 go 1.16
+
+require gonum.org/v1/gonum v0.9.1
--- nn/NeuronalNetwork.go
+++ nn/NeuronalNetwork.go
@@ -1,76 +1,23 @@
 package nn
 
-import (
-	"bufio"
-	"fmt"
-	"io/ioutil"
-	"os"
-)
+import "gonum.org/v1/gonum/mat"
 
-/*type Slice struct {
-	Id        int
-	attribute string
-}*/
-
-func LoadFile(pathName string) {
-
-	fileReader, err := ioutil.ReadFile(pathName)
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(string(fileReader))
-	//fmt.Println(reflect.ValueOf(fileReader).Kind())
-
+// Contain all the whole information about
+// The neural network trained
+type NeuralNet struct {
+	Config  NeuralNetConfig
+	Bhidden *mat.Dense
+	WHidden *mat.Dense
+	BOut    *mat.Dense
+	WOut    *mat.Dense
 }
 
-func LoopFile(fileName string) ([]string, error) {
-
-	var lines []string
-	file, err := os.Open(fileName)
-
-	if err != nil {
-
-		return nil, err
-	}
-
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		lines = append(lines, line)
-	}
-
-	err = file.Close()
-
-	if scanner.Err() != nil {
-		return nil, scanner.Err()
-	}
-
-	return lines, err
-
-}
-
-func GradientDescent() int {
-	return 0
-}
-
-// Metrics
-
-func EuclidianDistance() int {
-	return 0
-}
-
-func CosinDistance() int {
-	return 0
-}
-
-func ManhattanDistance() int {
-	return 0
-}
-
-func Greeting() {
-
-	fmt.Println("Hi")
+// NeuralNetconfig define the neural Network structure
+// Basically the architecture explained
+type NeuralNetConfig struct {
+	InputNeurons  int
+	HiddenNeurons int
+	OutputNeurons int
+	NumEpochs     int
+	LearningRate  float64
 }
