d35dd368caeb1e600429947aa51ebc510d628050
arturo <anegreiross@outlook.com>
2021-05-19 23:49:29 -0500

    reverse engineering to Neuronal Network

--- main.go
+++ main.go
@@ -1,25 +1,16 @@
 package main
 
 import (
-	"github.com/Arturo0911/NN/nn"
+	"fmt"
+
 	"gonum.org/v1/gonum/mat"
 )
 
 // @author: Arturo Negreiros (AKA Pxyl0xd)
 
-func main() {
+/*func main() {
 
 	// Initialize one neuron
-	neuralConf := nn.NeuralNetConfig{
-		InputNeurons:  3,
-		HiddenNeurons: 3,
-		OutputNeurons: 1,
-		NumberEpochs:  5000,
-		LearningRate:  0.3,
-	}
-
-	//var x *mat.Dense
-	//var y *mat.Dense
 
 	x := mat.NewDense(3, 4, []float64{
 		1.0, 0.0, 1.0, 0.0,
@@ -28,9 +19,40 @@ func main() {
 	})
 
 	y := mat.NewDense(3, 1, []float64{1.0, 1.0, 0.0})
+
+	neuralConf := nn.NeuralNetConfig{
+		InputNeurons:  4,
+		HiddenNeurons: 3,
+		OutputNeurons: 1,
+		NumberEpochs:  5000,
+		LearningRate:  0.3,
+	}
 	neuron := nn.NewNeuron1(neuralConf)
 	if err := neuron.Train(x, y); err != nil {
 		panic(err)
 	}
+
+	f := mat.Formatted(neuron.WHidden, mat.Prefix(" "))
+	fmt.Println(f)
+
+}*/
+
+func matPresentation(value *mat.Dense) {
+	element := mat.Formatted(value, mat.Prefix(" "))
+	fmt.Println(element)
+}
+
+func main() {
+	var output mat.Dense
+
+	x := mat.NewDense(2, 2, []float64{65, 43, 37, 66})
+	y := mat.NewDense(2, 2, []float64{35, 55, 98, 78})
+
+	fmt.Println("Showing matrix x and y respectevly")
+	matPresentation(x)
+	matPresentation(y)
+
+	output.MulElem(x, y)
+	matPresentation(&output)
 
 }
--- nn/neuron_1.go
+++ nn/neuron_1.go
@@ -1,11 +1,172 @@
 package nn
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
 
-func InitNeuron(inNeurons int) {
-	config := NeuralNetConfig{
-		InputNeurons: inNeurons,
+	"gonum.org/v1/gonum/floats"
+	"gonum.org/v1/gonum/mat"
+)
+
+// Initialize the neuron with the parameters inside
+func NewNeuron1(config NeuralNetConfig) *NeuralNet {
+	return &NeuralNet{
+		Config: config,
+	}
+}
+
+//Activation function => Sigmoid
+func Sigmoid(x float64) float64 {
+	return 1.0 / (1.0 + math.Exp(-x))
+}
+
+// derivative
+func SigmoidePrime(x float64) float64 {
+	return x * (1.0 - x)
+}
+
+func SumAlongAxis(axis int, m *mat.Dense) (*mat.Dense, error) {
+
+	numRows, numCols := m.Dims()
+	var output *mat.Dense
+
+	switch axis {
+	case 0:
+		data := make([]float64, numCols)
+		for i := 0; i < numCols; i++ {
+			col := mat.Col(nil, i, m)
+			data[i] = floats.Sum(col)
+		}
+		output = mat.NewDense(1, numCols, data)
+	case 1:
+		data := make([]float64, numRows)
+		for i := 0; i < numRows; i++ {
+			row := mat.Row(nil, i, m)
+			data[i] = floats.Sum(row)
+		}
+		output = mat.NewDense(numRows, 1, data)
+	default:
+		return nil, errors.New("invalid axis, should to be  1 or 0")
 	}
 
-	fmt.Println(config)
+	return output, nil
+}
+
+func (nn *NeuralNet) Train(x, y *mat.Dense) error {
+
+	// Initialize a number of weights and biases
+	randSource := rand.NewSource(time.Now().UnixNano())
+	randGen := rand.New(randSource)
+
+	wHiddenRaw := make([]float64, nn.Config.InputNeurons*nn.Config.HiddenNeurons)
+	wOutputRaw := make([]float64, nn.Config.HiddenNeurons*nn.Config.OutputNeurons)
+
+	bHiddenRaw := make([]float64, nn.Config.HiddenNeurons)
+	bOutputRaw := make([]float64, nn.Config.OutputNeurons)
+
+	// Fill into every array random numbers
+	for _, record := range [][]float64{wHiddenRaw, wOutputRaw, bHiddenRaw, bOutputRaw} {
+		for index := range record {
+			record[index] = randGen.Float64()
+		}
+	}
+
+	// Now create a NewDense for every parameter and add the respective array previously fill it
+	wHidden := mat.NewDense(nn.Config.InputNeurons, nn.Config.HiddenNeurons, wHiddenRaw)
+	wOutput := mat.NewDense(nn.Config.HiddenNeurons, nn.Config.OutputNeurons, wOutputRaw)
+	bHidden := mat.NewDense(1, nn.Config.HiddenNeurons, bHiddenRaw)
+	bOutput := mat.NewDense(1, nn.Config.OutputNeurons, bOutputRaw)
+
+	fmt.Println(nn)
+
+	var output mat.Dense
+
+	// Looping over the number of epochs
+	for i := 0; i < nn.Config.NumberEpochs; i++ {
+
+		var hiddenLayerInput mat.Dense
+
+		hiddenLayerInput.Mul(x, wHidden)
+
+		addBHidden := func(_, col int, value float64) float64 {
+			return (value + bHidden.At(0, col))
+		}
+		hiddenLayerInput.Apply(addBHidden, &hiddenLayerInput)
+
+		var hiddenLayerActivation mat.Dense
+		applySigmoid := func(_, _ int, value float64) float64 {
+			return Sigmoid(value)
+		}
+		hiddenLayerActivation.Apply(applySigmoid, &hiddenLayerInput)
+
+		var outputLayerInput mat.Dense
+		outputLayerInput.Mul(&hiddenLayerActivation, wOutput)
+		addBOut := func(_, col int, value float64) float64 {
+			return value + bOutput.At(0, col)
+		}
+		outputLayerInput.Apply(addBOut, &outputLayerInput)
+		output.Apply(applySigmoid, &outputLayerInput)
+
+		// Backpropagation
+
+		var networkErr mat.Dense
+		networkErr.Sub(y, &output)
+
+		var slopeOutputLayer mat.Dense
+		applySigmoidePrime := func(_, _ int, value float64) float64 {
+			return SigmoidePrime(value)
+		}
+		slopeOutputLayer.Apply(applySigmoidePrime, &output)
+
+		var slopeHiddenLayer mat.Dense
+		slopeHiddenLayer.Apply(applySigmoidePrime, &hiddenLayerActivation)
+
+		var dOutput mat.Dense
+		dOutput.MulElem(&networkErr, &slopeOutputLayer)
+
+		var errorAtHiddenLayer mat.Dense
+		errorAtHiddenLayer.Mul(&dOutput, wOutput.T())
+
+		var dHiddenLayer mat.Dense
+		dHiddenLayer.MulElem(&errorAtHiddenLayer, &slopeHiddenLayer)
+
+		// ADjust parameters
+
+		var wOutAdj mat.Dense
+		wOutAdj.Mul(hiddenLayerActivation.T(), &dOutput)
+		wOutAdj.Scale(nn.Config.LearningRate, &wOutAdj)
+		wOutput.Add(wOutput, &wOutAdj)
+
+		bOutAdj, err := SumAlongAxis(0, &dOutput)
+		if err != nil {
+			return err
+		}
+		bOutAdj.Scale(nn.Config.LearningRate, bOutAdj)
+		bOutAdj.Add(bOutput, bOutAdj)
+
+		var wHiddenAdj mat.Dense
+		wHiddenAdj.Mul(x.T(), &dHiddenLayer)
+		wHiddenAdj.Scale(nn.Config.LearningRate, &wHiddenAdj)
+		wHidden.Add(wHidden, &wHiddenAdj)
+
+		bHiddenAdj, err := SumAlongAxis(0, &dHiddenLayer)
+		if err != nil {
+			return err
+		}
+
+		bHiddenAdj.Scale(nn.Config.LearningRate, bHiddenAdj)
+		bHidden.Add(bHidden, bHiddenAdj)
+
+	}
+
+	nn.WHidden = wHidden
+	nn.BHidden = bHidden
+	nn.WOutput = wOutput
+	nn.BOutput = bOutput
+
+	return nil
+
 }
