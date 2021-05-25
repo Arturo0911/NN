ad8d45cc1d4a997b39c6dc9e278abc917bc2b4c2
Arturo Negreiros <anegreiross@outlook.com>
2021-05-25 00:45:17 -0500

    ok

--- go.mod
+++ go.mod
@@ -9,4 +9,5 @@ require (
 	golang.org/x/tools v0.1.1 // indirect
 	gonum.org/v1/gonum v0.9.1
 	gonum.org/v1/plot v0.9.0
+	gorgonia.org/gorgonia v0.9.17
 )
--- main.go
+++ main.go
@@ -1,40 +1,37 @@
 package main
 
-import "github.com/Arturo0911/NN/model/plotting"
+import (
+	"fmt"
+	"log"
+
+	"gorgonia.org/gorgonia"
+)
 
 // @author: Arturo Negreiros (AKA Pxyl0xd)
 
 func main() {
+	g := gorgonia.NewGraph()
 
-	// Initialize one neuron
+	var x, y, z *gorgonia.Node
+	var err error
 
-	/*x := mat.NewDense(3, 4, []float64{
-		1.0, 0.0, 1.0, 0.0,
-		1.0, 0.0, 1.0, 1.0,
-		0.0, 1.0, 0.0, 1.0,
-	})
-
-	y := mat.NewDense(3, 2, []float64{
-		1.0, 1.0,
-		0.0, 1.1,
-		2.1, 1.1})
-
-	neuralConf := nn.NeuralNetConfig{
-		InputNeurons:  4,
-		HiddenNeurons: 3,
-		OutputNeurons: 2,
-		NumberEpochs:  2000,
-		LearningRate:  0.3,
+	// Define the expression
+	x = gorgonia.NewScalar(g, gorgonia.Float64, gorgonia.WithName("x"))
+	y = gorgonia.NewScalar(g, gorgonia.Float64, gorgonia.WithName("y"))
+	if z, err = gorgonia.Add(x, y); err != nil {
+		log.Fatal(err)
 	}
-	neuron := nn.InitNeuron3(neuralConf)
-	if err := neuron.InitTraining(x, y); err != nil {
-		panic(err)
-	}*/
 
-	//if err := settings.MakingFiles("wineQualityReds.csv"); err != nil {
-	//	log.Fatal(err)
-	//}
+	// Create a VM to run the program on
+	machine := gorgonia.NewTapeMachine(g)
+	defer machine.Close()
 
-	plotting.CreatePloting("wineQualityReds.csv")
+	// Set initial alues then run
+	gorgonia.Let(x, 2.0)
+	gorgonia.Let(y, 2.5)
+	if err = machine.RunAll(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%v\n\n", z.Value())
 
 }
